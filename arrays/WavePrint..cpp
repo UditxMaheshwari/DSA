43c6fb582cd20dd86c35834c538f0adbde7cbf5e
Udit Maheshwari <167900008+UditxMaheshwari@users.noreply.github.com>
2025-09-15 01:44:07 +0530

    added row wise print to WavePrint

--- arrays/WavePrint..cpp
+++ arrays/WavePrint..cpp
@@ -1,3 +1,4 @@
+//column wise printing
 #include <vector>
 using namespace std;
 #include<iostream>
@@ -36,4 +37,48 @@ int main()
         return 0;
         
     }
+
+//row wise printing
+#include <vector>
+using namespace std;
+#include<iostream>
+
+void WavePrint(vector<vector<int>>v)
+{
+    int m=v.size();
+    
+    int n=v[0].size();
+   
+    for(int row=0;row<m;row++)
+    {
+        if((row &1)==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                cout << v[row][i]<<" ";
+            }
+        }
+            else
+            {
+                for(int j=n-1; j>=0;j--)
+                {
+                    cout << v[row][j]<<" ";
+                }
+            }
+        }
+    }
+
+int main()
+{
+    vector<vector<int>>v{
+        {1,2,3},
+        {5,6,7},
+        {9,10,11},
+        {13,14,15}};
+        WavePrint(v);
+        return 0;
+        
+    }
+
+
 
