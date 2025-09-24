1b3d569b8ddbbfe9ae01ff47e7ca1c81e9da6807
Udit Maheshwari <167900008+UditxMaheshwari@users.noreply.github.com>
2025-09-24 22:58:52 +0530

    Create remove element.cpp

---
+++ arrays/remove element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int start=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+           if(nums[i]!=val)
+           {
+              nums[start]=nums[i];
+              start++;
+           }
+        }
+        return start;
+    }
+};
