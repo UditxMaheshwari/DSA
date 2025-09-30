c3cc68f3022893035770314ba40fb88d6dec3e09
Udit Maheshwari <167900008+UditxMaheshwari@users.noreply.github.com>
2025-10-01 04:24:59 +0530

    Create Max subarray(LSCS) by Kadane's Algorithm.cpp

---
+++ arrays/Max subarray(LSCS) by Kadane's Algorithm.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum=INT_MIN;
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+
+            if(sum>maxsum)
+            {
+                maxsum=sum;
+            }
+            
+           if(sum<0)
+           {
+            sum=0;
+           }
+        }
+        return maxsum;
+    }
+};
